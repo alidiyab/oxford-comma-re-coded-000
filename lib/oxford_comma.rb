475e7635b4479ed16a6b13394b646ba73382fba6
Ali Diyab <alidiyab1997@gmail.com>
2016-12-20 18:44:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
-end
+  if array.size>2
+     last = array.pop
+   array.join(", ") + ", and " + last.to_s
+ elsif array.size==2
+     array.join(" and ")
+     elsif
+       array.size==1
+       array.join(" ")
+   end
+ end
